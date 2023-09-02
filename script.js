4061a7afe4bea314b966c5afea7b6be0a8e1c4f8
Kevin <kevinkurniawan265@gmail.com>
2023-09-02 15:26:07 +0700

    fix download button

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+// Typing Animation
 var typed = new Typed('#typing-text', {
     strings: ["Programmer", "Web Developer", "UI/UX Designer"],
     typeSpeed: 50,
     backSpeed: 50,
     loop: true,
+});
+
+// Download Button
+// Dapatkan referensi ke tombol unduhan
+var downloadButton = document.getElementById('downloadBtn');
+
+// Tambahkan event listener untuk mendeteksi klik tombol
+downloadButton.addEventListener('click', function() {
+  // Ganti URL di bawah dengan URL file PDF yang ingin Anda unduh
+  var pdfUrl = 'Resume Kevin Kurniawan Handoko.pdf';
+
+  // Buat elemen anchor untuk menginisiasi unduhan
+  var link = document.createElement('a');
+  link.href = pdfUrl;
+  link.download = 'Resume Kevin Kurniawan Handoko.pdf'; // Nama file yang akan disimpan oleh pengguna
+  link.target = '_blank'; // Untuk membuka link di jendela/tab baru jika diperlukan
+
+  // Simulasikan klik pada link untuk memulai unduhan
+  link.click();
 });
