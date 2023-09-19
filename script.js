f4e5a6da8215e4a66f1099a215fab1e98d215dd5
Kevin <kevinkurniawan265@gmail.com>
2023-09-19 11:48:59 +0700

    change CV file

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ var downloadButton = document.getElementById("downloadBtn");
 // Tambahkan event listener untuk mendeteksi klik tombol
 downloadButton.addEventListener("click", function () {
   // Ganti URL di bawah dengan URL file PDF yang ingin Anda unduh
-  var pdfUrl = "Resume Kevin Kurniawan Handoko.pdf";
+  var pdfUrl = "CV Kevin Kurniawan Handoko.pdf";
 
   // Buat elemen anchor untuk menginisiasi unduhan
   var link = document.createElement("a");
   link.href = pdfUrl;
-  link.download = "Resume Kevin Kurniawan Handoko.pdf"; // Nama file yang akan disimpan oleh pengguna
+  link.download = "CV Kevin Kurniawan Handoko.pdf"; // Nama file yang akan disimpan oleh pengguna
   link.target = "_blank"; // Untuk membuka link di jendela/tab baru jika diperlukan
 
   // Simulasikan klik pada link untuk memulai unduhan
